317e2e5e2fc4dcd0f02211f2de68dc4118805d83
Julian Doucette <j.doucette@eyeo.com>
2017-08-17 18:25:25 +0200

    Issue 5447 - Add missing essential meta data to adblockplus.org

---
+++ filters/to_og_locale.py
@@ -0,0 +1,33 @@
+og_locales = {
+    'ar': 'ar_AR',
+    'bg': 'bg_BG',
+    'de': 'de_DE',
+    'dk': 'da_DK',
+    'da': 'da_DK',
+    'en': 'en_US',
+    'es': 'es_LA',
+    'fr': 'fr_FR',
+    'el': 'el_GR',
+    'he': 'he_IL',
+    'hu': 'hu_HU',
+    'it': 'it_IT',
+    'ja': 'ja_JP',
+    'ko': 'ko_KR',
+    'lt': 'lt_LT',
+    'nl': 'nl_NL',
+    'pl': 'pl_PL',
+    'pt': 'pt_PT',
+    'pt_BR': 'pt_BR',
+    'ro': 'ro_RO',
+    'ru': 'ru_RU',
+    'sk': 'sk_SK',
+    'sl': 'sl_SI',
+    'sv': 'sv_SE',
+    'tr': 'tr_TR',
+    'vi': 'vi_VN',
+    'zh_CN': 'zh_CN',
+    'zh_TW': 'zh_TW',
+}
+
+def to_og_locale(locale):
+    return og_locales[locale]
