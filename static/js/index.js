d9ac678f37c27710118482a533d383e61e08302b
Ire Aderinokun <i.aderinokun@eyeo.com>
2017-10-16 14:39:58 +0100

    Issue 5727 - Implement non-embedded homepage video on abp.org

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,7 @@
 (function()
 {
+  document.documentElement.className = "js";
+
   var visibleTab;
   var container = document.getElementById("more-container");
 
@@ -25,4 +27,51 @@
     visibleTab = ua && ua[1] || "firefox";
     return visibleTab;
   }
+
+  // Change overlay icon on #video to play icon
+  document
+    .getElementById("video-play")
+    .setAttribute("src", "/img/video-play.png");
+
+  // Save time user clicked to show video disclaimer
+  var timeClickedVideo;
+
+  function changeImageToVideo(event)
+  {
+    event.preventDefault();
+    
+    var videoContainer = document.getElementById("video-container");
+
+    if (videoContainer.className == "show-disclaimer")
+    {
+      // Enfore 600ms delay
+      var currentTime = new Date().getTime();
+      if (currentTime - timeClickedVideo < 600) return;
+
+      var image = this;
+      
+      var video = document.createElement("iframe");
+      video.id = "video";
+      video.setAttribute("frameborder", "0");
+      video.setAttribute("height", "285");
+      video.setAttribute("width", "520");
+      video.setAttribute("itemprop", "video");
+      video.setAttribute("allowfullscreen", "allowfullscreen");
+      video.setAttribute("src", image.getAttribute("href"));
+      
+      image.parentNode.replaceChild(video, image);
+
+      videoContainer.className = "";
+    }
+    else
+    {
+      videoContainer.className = "show-disclaimer";
+      timeClickedVideo = new Date().getTime();
+    }
+  }
+
+  document
+    .getElementById("video")
+    .addEventListener("click", changeImageToVideo);
+
 })();
