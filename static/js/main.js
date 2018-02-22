afa1aa60dde0673d003f834a0169ac473a7cd916
Yael Stein <yaelst@gmail.com>
2018-02-22 10:40:27 +0100

    navbar disppears when scrolled up, reappears when scrolled up

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,62 @@
 "use strict";
 
-(function()
-{
-  function initLanguageSelection()
-  {
-    var locale = document.getElementById("navbar-locale-selected");
+(function() {
+    function initLanguageSelection() {
+        var locale = document.getElementById("navbar-locale-selected");
 
-    // skip if page does not have language selection (EG: blog)
-    if (!locale) return;
+        // skip if page does not have language selection (EG: blog)
+        if (!locale) return;
 
-    locale.addEventListener("click", function()
-    {
-      document.getElementById("navbar-locale-menu")
-        .classList.toggle("visible");
-    }, false);
-  }
+        locale.addEventListener("click", function() {
+            document.getElementById("navbar-locale-menu")
+                .classList.toggle("visible");
+        }, false);
+    }
 
-  function navigationClick(event)
-  {
-    document.getElementById("navbar-menu")
-      .classList.toggle("visible");
-  }
+    function navigationClick(event) {
+        document.getElementById("navbar-menu")
+            .classList.toggle("visible");
+    }
 
-  function initMenu()
-  {
-    document.getElementById("navbar-menu-toggle")
-      .addEventListener("click", navigationClick, false);
-  }
+    // Hide navbar on scroll down, show on scroll up
+    var navBar = document.getElementById("navbar");
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = navBar.clientHeight;
 
-  initLanguageSelection();
-  initMenu();
+    window.addEventListener("scroll", (function() {
+        didScroll = true;
+    }));
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = window.scrollY;
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+            // Scroll Down
+        if (st > lastScrollTop && st > navbarHeight) {
+            navBar.classList.add("hide-navbar");
+        } else {
+            // Scroll Up
+            if (st + window.innerHeight < document.body.clientHeight) {
+                navBar.classList.remove("hide-navbar");
+            }
+        }
+        lastScrollTop = st;
+    }
+
+    function initMenu() {
+        document.getElementById("navbar-menu-toggle")
+            .addEventListener("click", navigationClick, false);
+    }
+
+    initLanguageSelection();
+    initMenu();
 })();
