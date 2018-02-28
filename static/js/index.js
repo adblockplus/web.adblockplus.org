56112eb2390960476a5f38b43e407828771e5fe2
Julian Doucette <j.doucette@eyeo.com>
2018-02-28 20:10:19 +0000

    #26 - Abstracted home page video component

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,5 @@
 (function()
 {
-  document.documentElement.className = "js";
-
   var visibleTab;
   var container = document.getElementById("more-container");
 
@@ -27,51 +25,5 @@
     visibleTab = ua && ua[1] || "firefox";
     return visibleTab;
   }
-
-  // Change overlay icon on #video to play icon
-  document
-    .getElementById("video-play")
-    .setAttribute("src", "/img/video-play.png");
-
-  // Save time user clicked to show video disclaimer
-  var timeClickedVideo;
-
-  function changeImageToVideo(event)
-  {
-    event.preventDefault();
-    
-    var videoContainer = document.getElementById("video-container");
-
-    if (videoContainer.className == "show-disclaimer")
-    {
-      // Enfore 600ms delay
-      var currentTime = new Date().getTime();
-      if (currentTime - timeClickedVideo < 600) return;
-
-      var image = this;
-      
-      var video = document.createElement("iframe");
-      video.id = "video";
-      video.setAttribute("frameborder", "0");
-      video.setAttribute("height", "285");
-      video.setAttribute("width", "520");
-      video.setAttribute("itemprop", "video");
-      video.setAttribute("allowfullscreen", "allowfullscreen");
-      video.setAttribute("src", image.getAttribute("href"));
-      
-      image.parentNode.replaceChild(video, image);
-
-      videoContainer.className = "";
-    }
-    else
-    {
-      videoContainer.className = "show-disclaimer";
-      timeClickedVideo = new Date().getTime();
-    }
-  }
-
-  document
-    .getElementById("video")
-    .addEventListener("click", changeImageToVideo);
 
 })();
--- static/js/main.js
+++ static/js/main.js
@@ -31,3 +31,100 @@
   initLanguageSelection();
   initMenu();
 })();
+
+(function()
+{
+  /**
+   * Creats a GDPR compatible video
+   * @constructor
+   * @param {Element} parent - video parent / container
+   * @example
+   * <div id="example-video" class="video-parent">
+   *   <a
+   *     class="video-link"
+   *     target="_blank"
+   *     href="http://example.com/iframe/video/src">
+   *     <img
+   *       class="video-thumbnail"
+   *       alt="Short description of video"
+   *       src="/img/video-thumbnail.png" />
+   *     <img
+   *       class="video-play"
+   *       alt="Open video in separate window""
+   *       src="/img/video-link.png" />
+   *   </a>
+   *   <p class="video-disclaimer">Disclaimer text</p>
+   * </div>
+   */
+  function Video(parent)
+  {
+    /**
+     * Video parent / container element
+     * @member {Element}
+     */
+    this.parent = parent;
+
+    /**
+     * The last time that the play button was clicked
+     * @member {number}
+     */
+    this.lastClicked = 0;
+
+    var videoLink = parent.querySelector(".video-link");
+
+    /**
+     * The iframe video src url
+     * @member {string}
+     */
+    this.src = videoLink.getAttribute("href");
+
+    //remove disclaimer if disclaimer is shown by default
+    this.parent.classList.add("hide-disclaimer");
+
+    //change external link icon into play button icon
+    parent.querySelector(".video-play")
+      .setAttribute("src", "/img/video-play.png");
+
+    //show disclaimer or replace thumbnail with video on click
+    videoLink.addEventListener("click", this._onPlayClick.bind(this));
+  }
+
+  Video.prototype = {
+
+    _onPlayClick: function(event)
+    {
+      event.preventDefault();
+
+      if (this.parent.classList.contains("hide-disclaimer"))
+        this.toggleDisclaimer();
+      //prevent bypassing the disclaimer via double click
+      else if (new Date().getTime() - this.lastClicked > 600)
+        this.insertVideo();
+    },
+
+    toggleDisclaimer: function()
+    {
+      this.parent.classList.toggle("hide-disclaimer");
+      this.lastClicked = new Date().getTime();
+    },
+
+    /** Replace video thumbnail with video iframe */
+    insertVideo: function()
+    {
+      this.parent.classList.add("hide-disclaimer");
+      this.parent.classList.add("has-iframe");
+
+      //replace static thumbnail with iframe video
+      this.parent.innerHTML =
+        "<iframe " +
+          "class='video-iframe' " +
+          "frameborder=0 " +
+          "allowfullscreen " +
+          "src='" + encodeURI(this.src) + "'>" +
+        "</iframe>";
+    }
+  };
+
+  window.videos = [].slice.call(document.querySelectorAll(".video-parent"))
+    .map(function(parent) {return new Video(parent);});
+}());
