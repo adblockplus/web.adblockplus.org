97a26212598091e99282db4aeaead50ab48dfcb9
Julian Doucette <j.doucette@eyeo.com>
2018-12-17 17:01:24 +0000

    Refs #141 - Fixed issue with deep link parameters introduced in tracking.js in 604

--- static/js/tracking.js
+++ static/js/tracking.js
@@ -43,7 +43,7 @@ document.addEventListener("click", function(event)
   var siteURL = document.documentElement.getAttribute('data-siteurl');
 
   // if is internal link
-  if (event.target.origin === siteURL) {
+  if (event.target.origin === siteURL && event.target.href.indexOf("#") === -1) {
     // pass along url parameters
     event.target.href = event.target.href += "?" + (window.location.href.split("?")[1] || "");
   }
