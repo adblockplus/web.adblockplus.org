399c57ef70252456a1f783157dcd4167c9c2aa6e
Julian Doucette <julian@adblockplus.org>
2020-03-17 11:06:51 +0000

    Refd #408 - Implemented paypal provider for update page donation form

--- static/js/update.js
+++ static/js/update.js
@@ -1,9 +1,14 @@
-/*global PaymentForm*/
+/*global PaymentForm, paypalProvider*/
 (function()
 {
   function setupPaymentForm()
   {
     var form = new PaymentForm();
+
+    form.addProviderListener("paypal", function()
+    {
+      paypalProvider.submit(form.toJSON());
+    });
   }
 
   if (document.readyState === 'loading')
