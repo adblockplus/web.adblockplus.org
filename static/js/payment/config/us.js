05dc10e139cf0af5dc0d249caea9fb71a934667b
Julian Doucette <julian@adblockplus.org>
2020-03-22 15:45:15 +0000

    Refd #394 - Implemented minimum amount validation and warnings in payment form on update page

--- static/js/payment/config/us.js
+++ static/js/payment/config/us.js
@@ -4,10 +4,12 @@ window.paymentConfig = {
     donation: {
       amounts: [10, 15, 20, 35, 50],
       placeholder: 35,
+      minimum: 5
     },
     subscription: {
       amounts: [1.99, 2.99, 3.99, 4.99, 9.99],
-      placeholder: 4.99
+      placeholder: 4.99,
+      minimum: 1
     }
   }
 };
