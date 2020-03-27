e5430bb560da50db4c50f1b51f2951be1f5f8ba7
Chris Hart <c.hart@eyeo.com>
2020-03-27 15:48:38 +0000

    Revert "stripe input font size reduction to accommodate postal code placeholder"
    This reverts commit f782633b125f43f5fd13964acd8033ed2333ac9e

--- static/js/payment/providers/stripe.js
+++ static/js/payment/providers/stripe.js
@@ -26,7 +26,7 @@ function initStripeProvider(publishableKey, formProcessor, dictionary) {
       fontFamily:
         '-apple-system, BlinkMacSystemFont, system-ui, sans-serif',
       fontSmoothing: 'antialiased',
-      fontSize: '15px',
+      fontSize: '16px',
       '::placeholder': { color: '#aab7c4' }
     },
     invalid: {
