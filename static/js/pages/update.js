8d3cd2ce43a070fecf75e40a14b1be85451f01c9
Julian Doucette <j.doucette@eyeo.com>
2020-08-10 12:48:48 +0100

    Refd #496 - Re-added stripe provider to payment forms (backed out of d1d13680f5bd)

--- static/js/pages/update.js
+++ static/js/pages/update.js
@@ -10,13 +10,47 @@ function setupPaymentForm()
     .getAttribute("data-siteurl") || "https://adblockplus.org";
 
   var successURL = siteURL + "/update-payment-complete";
- 
+
+  var stripeLoaded = false;
+
   form.addProviderListener("paypal", function()
   {
     var payment = form.toJSON();
     payment.item = paymentTranslations.item;
     payment.successURL = successURL;
     paypalProvider.submit(payment);
+  });
+
+  function onStripeSubmit()
+  {
+    var payment = form.toJSON();
+    payment.currencySign = paymentConfig[payment.currency.toUpperCase()].sign;
+    payment.successURL = successURL;
+    stripeProvider.submit(payment);
+  }
+
+  form.addProviderListener("stripe", function()
+  {
+    if (!stripeLoaded)
+    {
+      var button = document.querySelector(".stripe-button");
+      var buttonContent = button.innerHTML;
+      button.disabled = true;
+      button.innerHTML = "<div class='loader'>Loading...</div>";
+      var script = document.createElement("script");
+      script.onload = function() {
+        stripeLoaded = true;
+        onStripeSubmit();
+        button.disabled = false;
+        button.innerHTML = buttonContent;
+      };
+      script.src = "https://js.stripe.com/v3/";
+      document.head.appendChild(script);
+    }
+    else
+    {
+      onStripeSubmit();
+    }
   });
 }
 
