49fee76a1a20a681d1e07d391b35ba6379319efe
Julian Doucette <j.doucette@eyeo.com>
2020-09-09 13:04:50 +0100

    Refd #507 - Renamed tracking scripts to avoid false positives

--- static/js/tracking-eea.js
+++
@@ -1,6 +0,0 @@
-/**
- * We don't track devices in the EEA
- *
- * Caution: This file exists for backwards compatibility during infrastructure change. It's successor is/will be /js/tracking/tracking.js
- */
-document.documentElement.classList.remove('async-hide');
