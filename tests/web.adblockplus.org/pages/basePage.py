23b3962e3fc342088940b2e8353acf376f3796fa
Dorel Gabor <d.gabor@eyeo.com>
2020-10-15 11:33:00 +0300

    Refd #565 - Added test cases for download links

--- tests/web.adblockplus.org/pages/basePage.py
+++ tests/web.adblockplus.org/pages/basePage.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+
+
 class BasePage:
 
     def __init__(self, driver):
@@ -8,4 +12,8 @@ class BasePage:
 
     def go_home(self):
         self.driver.get('http://adblockplus.org/')
+
+    def is_redirect_to_url(self, url):
+        wait = WebDriverWait(self.driver, 10)
+        return wait.until(ec.url_to_be(url))
 
