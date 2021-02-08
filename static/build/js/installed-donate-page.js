3fbb25cf075dfdd6d0c7e51eef3ccd631e5588a3
Tommaso Romito <t.romito@eyeo.com>
2021-02-08 11:47:26 +0100

    Refd #628 - Created a switchable FRP UX used for A/B tests

--- static/build/js/installed-donate-page.js
+++ static/build/js/installed-donate-page.js
@@ -1607,6 +1607,35 @@ var navbarDonateButton = document.querySelector('.scroll-to-donate');
 var donateSection = document.querySelector('.donate');
 var abbSection = page.querySelector('.abb');
 
+// sticky footer (here only cause the A/B test #628)
+document.querySelector('main.container').setAttribute('id', 'content');
+
+// Detatched Donation form (here only cause the A/B test #628)
+var donateSectionHtml = donateSection.innerHTML;
+
+var newDonateSectionHtml = '<div class="fake-container">' + donateSectionHtml + '</div>';
+
+donateSection.innerHTML = newDonateSectionHtml;
+
+var closeFormButton = document.createElement('a');
+
+closeFormButton.setAttribute('id', 'close-form-button');
+
+closeFormButton.addEventListener('click', function() {
+  page.classList.remove('show-form');
+  page.classList.add('hide-form');
+})
+
+donateSection.appendChild(closeFormButton);
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth > 991)
+    if (!page.classList.contains('hide-form'))
+      page.classList.add('show-form');
+  else
+    page.classList.remove('show-form');
+});
+
 document.documentElement.classList.remove('no-js');
 
 navbarDonateButton
