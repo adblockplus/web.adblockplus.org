7d7a50c9f8bbc6d69b714699c785bbca693cde86
Chris Hart <c.hart@eyeo.com>
2021-05-21 19:42:54 +0100

    Refd #715 - Introduced yearly FRP contribution options in split test

--- static/build/js/installed-715.js
+++ static/build/js/installed-715.js
@@ -650,7 +650,6 @@ Lt(r,Rr(r),t,e)}),Qu=mt(function(r,n){try{return t(r,Vr,n)}catch(t){return gr(t)
 m.isPlainObject=xr,m.isSet=Gu,m.isString=zr,m.isSymbol=Sr,m.isTypedArray=Hu,m.stubArray=Dr,m.stubFalse=Wr,m.template=Mr,m.toNumber=$r,m.toString=kr,m.each=_r,m.VERSION=qr,Ue._=m}).call(this);
 /*global PaymentForm, updatePaymentConfig, _*/
 (function(root, doc, _){
-
 /**
  * Construct payment form
  * @constructor
@@ -664,6 +663,9 @@ m.isPlainObject=xr,m.isSet=Gu,m.isString=zr,m.isSymbol=Sr,m.isTypedArray=Hu,m.st
  * @param {Object=} currencies[].subscription - (optional) monthly payment options for currency
  * @param {Number[]} currencies[].subscription.amounts - Amounts offered for monthly payment
  * @param {Number} currencies[].subscription.placeholder - Custom amount placeholder for monthly payment
+ * @param {Object=} currencies[].yearlySubscription - (optional) yearly payment options for currency
+ * @param {Number[]} currencies[].yearlySubscription.amounts - Amounts offered for yearly payment
+ * @param {Number} currencies[].yearlySubscription.placeholder - Custom amount placeholder for yearly payment
  */
 function PaymentForm(currencies)
 {
@@ -706,6 +708,9 @@ function PaymentForm(currencies)
 
   var subscriptionAmounts = doc.getElementById("subscription-amounts");
 
+  var yearlySubscriptionAmounts =
+    doc.getElementById("yearly-subscription-amounts");
+
   var thePresetAmounts = _.template(
     doc.getElementById("preset-payment-amounts").innerHTML
   );
@@ -732,9 +737,10 @@ function PaymentForm(currencies)
     if (currency.subscription)
     {
       doc.body.classList.add("has-subscriptions");
+      doc.body.classList.add("has-yearly");
 
       var subscriptionOptions = {
-        type: "subscription",
+        type: "monthly-subscription",
         sign: currency.sign,
         amounts: currency.subscription.amounts,
         placeholder: currency.subscription.placeholder
@@ -743,6 +749,22 @@ function PaymentForm(currencies)
       subscriptionAmounts.innerHTML = ""
         + thePresetAmounts(subscriptionOptions)
         + theCustomAmount(subscriptionOptions);
+      
+      if (currency.yearly) {
+        var yearlySubscriptionOptions = {
+          type: "yearly-subscription",
+          sign: currency.sign,
+          amounts: currency.yearly.amounts,
+          placeholder: currency.yearly.placeholder
+        };
+
+        yearlySubscriptionAmounts.innerHTML = ""
+          + thePresetAmounts(yearlySubscriptionOptions)
+          + theCustomAmount(yearlySubscriptionOptions);
+
+      } else {
+        doc.body.classList.remove("has-yearly");
+      } 
     }
     else
     {
@@ -758,8 +780,8 @@ function PaymentForm(currencies)
     validateCustomAmount(e);
   });
 
-  // uncheck donation amount when subscription amount is selected and vise versa
-  function onFieldsetChange (otherFieldset, event)
+  // uncheck donation amount when subscription amount is selected and vice versa
+  function onFieldsetChange(otherFieldset, event)
   {
     var otherFieldsetSelected = otherFieldset.querySelector("input:checked");
 
@@ -772,12 +794,30 @@ function PaymentForm(currencies)
 
   donationAmounts.addEventListener(
     "change",
-    onFieldsetChange.bind(this, subscriptionAmounts)
-  );
+    onFieldsetChange.bind(this, subscriptionAmounts));
+
+  donationAmounts.addEventListener(
+    "change",
+    onFieldsetChange.bind(this, yearlySubscriptionAmounts));
 
   subscriptionAmounts.addEventListener(
     "change",
     onFieldsetChange.bind(this, donationAmounts)
+  );
+
+  subscriptionAmounts.addEventListener(
+    "change",
+    onFieldsetChange.bind(this, yearlySubscriptionAmounts)
+  );
+
+  yearlySubscriptionAmounts.addEventListener(
+    "change",
+    onFieldsetChange.bind(this, donationAmounts)
+  );
+
+  yearlySubscriptionAmounts.addEventListener(
+    "change",
+    onFieldsetChange.bind(this, subscriptionAmounts)
   );
 
   // Select custom amount radio when textbox is focused
@@ -794,10 +834,13 @@ function PaymentForm(currencies)
 
   subscriptionAmounts.addEventListener("focus", onCustomFieldSelect, true);
 
+  yearlySubscriptionAmounts.addEventListener("focus", onCustomFieldSelect, true);
+
   function clearCustomAmountErrors()
   {
     doc.body.classList.remove("minimum-donation-error");
     doc.body.classList.remove("minimum-subscription-error");
+    doc.body.classList.remove("minimum-yearly-error");
 
     enableForm(true);
   }
@@ -817,7 +860,21 @@ function PaymentForm(currencies)
   {
     amount = parseFloat(amount);
 
+    type = (type == 'yearly-subscription') ? 'yearly' : type;
+
+    type = (type == 'monthly-subscription') ? 'subscription' : type;
+
     return _.isFinite(amount) && amount >= currency[type].minimum;
+  }
+
+  /*function isSubscription(type) {
+    return type == "subscription" || type == "yearly";
+  }*/
+
+  function otherTypes(type) {
+    return ["donation", "subscription", "yearly"].filter(function(item) {
+      return item != type;
+    });
   }
 
   function validateCustomAmount(event)
@@ -826,8 +883,7 @@ function PaymentForm(currencies)
 
     if (checkedRadio.value != "custom")
     {
-      clearCustomAmountErrors();
-      return;
+      return clearCustomAmountErrors();
     }
 
     var amount = checkedRadio.parentElement.querySelector(
@@ -836,17 +892,14 @@ function PaymentForm(currencies)
 
     if (amount.trim() == "")
     {
-      clearCustomAmountErrors();
-      return;
+      return clearCustomAmountErrors();
     }
 
     amount = parseFloat(amount);
 
     var currency = currencies[paymentCurrency.value];
     var selectedType = event.currentTarget.id.split("-")[0];
-    var otherType = selectedType == "donation" ? "subscription" : "donation";
     var typeError = "minimum-" + selectedType + "-error";
-    var otherTypeError = "minimum-" + otherType + "-error";
     var minimumAmount = currency[selectedType].minimum;
 
     if (isValidAmount(amount, selectedType, currency))
@@ -855,18 +908,42 @@ function PaymentForm(currencies)
       return;
     }
 
+    otherTypes(selectedType).forEach(function(otherType) {
+      doc.body.classList.remove("minimum-" + otherType + "-error");
+    });
+    
     doc.body.classList.add(typeError);
-    doc.body.classList.remove(otherTypeError);
+    
     enableForm(false);
+
     doc.querySelector(
       ".minimum-" + selectedType + "-warning .minimum-amount"
     ).textContent = currency.sign + minimumAmount;
   }
 
+  function actionType(text) {
+    var type;
+
+    if (/donation/.test(text)) {
+      type = 'donation';
+
+    } else if (/monthly/.test(text)) {
+      type = 'monthly-subscription';
+
+    } else if (/yearly/.test(text)) {
+      type = 'yearly-subscription';
+    }
+
+    return type;
+  }
+
   donationAmounts.addEventListener("change", validateCustomAmount, true);
   subscriptionAmounts.addEventListener("change", validateCustomAmount, true);
+  yearlySubscriptionAmounts.addEventListener("change", validateCustomAmount, true);
+  
   donationAmounts.addEventListener("input", validateCustomAmount, true);
   subscriptionAmounts.addEventListener("input", validateCustomAmount, true);
+  yearlySubscriptionAmounts.addEventListener("input", validateCustomAmount, true);
 
   /**
    * Export form data to JSON compatible object
@@ -878,8 +955,7 @@ function PaymentForm(currencies)
 
     var checked = doc.querySelector(".payment-amount input[type=radio]:checked");
 
-    var type = checked.name.indexOf("donation") != -1 ?
-      "donation" : "subscription";
+    var type = actionType(checked.name);
 
     var amount = checked.value;
 
@@ -1615,47 +1691,3 @@ document.documentElement.classList.remove('no-js');
 
 // sticky footer
 document.querySelector('main.container').setAttribute('id', 'content');
-
-/*!
- * This file is part of website-defaults
- * Copyright (C) 2016-present eyeo GmbH
- *
- * website-defaults is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * website-defaults is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with website-defaults.  If not, see <http://www.gnu.org/licenses/>.
- */
-var ADDRESS_MASKING_DELAY = 250;
-
-function unmaskAddress(target)
-{
-  var attributes = JSON.parse(target.getAttribute("data-mask"));
-
-  for (var attr in attributes)
-    target[attr] = atob(attributes[attr]);
-
-  target.removeAttribute("data-mask");
-}
-
-document.addEventListener("DOMContentLoaded", function()
-{
-  var unmaskAfterTimeout = setTimeout.bind(
-    this,
-    unmaskAddress,
-    ADDRESS_MASKING_DELAY
-  );
-
-  var linksToBeMasked = [].slice.call(
-    document.querySelectorAll("[data-mask]")
-  );
-
-  linksToBeMasked.forEach(unmaskAddress);
-});
