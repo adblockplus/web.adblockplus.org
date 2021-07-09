b14f68c563b662eb235347d5364212ee55abc1c4
Chris Hart <c.hart@adblock.org>
2021-07-09 23:53:15 +0200

    Refd #629

--- static/js/payment/config/eu.js
+++ static/js/payment/config/eu.js
@@ -5,11 +5,31 @@ window.paymentConfig = {
       amounts: [10, 15, 20, 35, 50],
       placeholder: 35,
       minimum: 5
+    },
+    subscription: {
+      amounts: [1.99, 2.99, 3.99, 4.99, 9.99],
+      placeholder: 4.99,
+      minimum: 1
+    },
+    yearly: {
+      amounts: [10, 15, 20, 35, 50],
+      placeholder: 35,
+      minimum: 5
     }
   },
   GBP: {
     sign: "£",
     donation: {
+      amounts: [10, 15, 20, 35, 50],
+      placeholder: 35,
+      minimum: 5
+    },
+    subscription: {
+      amounts: [1.99, 2.99, 3.99, 4.99, 9.99],
+      placeholder: 4.99,
+      minimum: 1
+    },
+    yearly: {
       amounts: [10, 15, 20, 35, 50],
       placeholder: 35,
       minimum: 5
@@ -21,6 +41,32 @@ window.paymentConfig = {
       amounts: [10, 15, 20, 35, 50],
       placeholder: 35,
       minimum: 5
+    },
+    subscription: {
+      amounts: [1.99, 2.99, 3.99, 4.99, 9.99],
+      placeholder: 4.99,
+      minimum: 1
+    },
+    yearly: {
+      amounts: [10, 15, 20, 35, 50],
+      placeholder: 35,
+      minimum: 5
+    }
+  },
+  JPY: {
+    sign: "¥",
+    donation: {
+      amounts: [500, 1000, 1500, 3000, 5000, 10000],
+      placeholder: 3000,
+      minimum: 500
+    }
+  },
+  RUB: {
+    sign: "₽",
+    donation: {
+      amounts: [250, 400, 500, 1000, 2000, 5000],
+      placeholder: 1000,
+      minimum: 250
     }
   }
 };
