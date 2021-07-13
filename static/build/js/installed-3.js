8cca08865b95d1d8a1e923e7781aeb3439d2f9f1
Tommaso Romito <t.romito@eyeo.com>
2021-07-13 12:34:48 +0200

    Refd #717 - Remove the split test

--- static/build/js/installed-3.js
+++ static/build/js/installed-3.js
@@ -1470,7 +1470,7 @@ function initStripeProvider(publishableKey, formProcessor, text) {
     }
 
     function cardBrand(brand) {
-      if (['diners', 'discover', 'jcb', 'unionpay'].indexOf(brand) != -1)
+      if (['visa', 'mastercard', 'amex'].indexOf(brand) == -1)
         errorText(text.notSupported);
     }
 
