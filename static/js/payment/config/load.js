8e9a174dc298a41886ebcd60e8325c4597722e3b
Chris Hart <c.hart@adblock.org>
2021-08-07 15:20:43 +0200

    Refd #769 - Added yearly options to the donate page

--- static/js/payment/config/load.js
+++ static/js/payment/config/load.js
@@ -10,6 +10,11 @@ window.paymentConfig = {
       amounts: [1.99, 2.99, 3.99, 4.99, 9.99],
       placeholder: 4.99,
       minimum: 1
+    },
+    yearly: {
+      amounts: [10, 15, 20, 35, 50],
+      placeholder: 35,
+      minimum: 5
     }
   },
   EUR: {
@@ -23,6 +28,11 @@ window.paymentConfig = {
       amounts: [1.99, 2.99, 3.99, 4.99, 9.99],
       placeholder: 4.99,
       minimum: 1
+    },
+    yearly: {
+      amounts: [10, 15, 20, 35, 50],
+      placeholder: 35,
+      minimum: 5
     }
   }
 };
