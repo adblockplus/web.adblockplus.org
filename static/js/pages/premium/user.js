8cd0bb7df3e8ca8c5496f14a294d1198a9dea172
Julian Doucette <julian@adblock.org>
2022-07-11 16:05:17 +0000

    Fixed issue with user

---
+++ static/js/pages/premium/user.js
@@ -0,0 +1,415 @@
+// Returns the adblock userid, if known
+function getUserId() {
+    var _userIdOptions = [
+      (document.location.search.match(/(?:[?&])u=([a-zA-Z0-9]+)/) || {})[1],
+      typeof adblock_userid !== "undefined" ? adblock_userid : undefined,
+      (document.getElementById('adblock_user_id') || {}).innerText,
+      ""
+    ];
+    // Use the first one that has a value.
+    return _userIdOptions.filter(function(o) { return o !== undefined; })[0];
+}
+
+// Returns the adblock userid if exists, else return unknown
+function getUserIdOrUnknown() {
+    var userId = getUserId();
+    return userId === "" ? "unknown" : userId;
+}
+
+// Returns the adblock premium userid, if known
+function getPremiumUserId() {
+    var _userIdOptions = [
+      (document.location.search.match(/(?:[?&])u=([a-zA-Z0-9]+)/) || {})[1],
+      typeof adblock_premium_userid !== "undefined" ? adblock_premium_userid : undefined,
+      (document.getElementById('adblock_premium_user_id') || {}).innerText,
+      ""
+    ];
+    // Use the first one that has a value.
+    return _userIdOptions.filter(function(o) { return o !== undefined; })[0];
+}
+
+// Returns the adblock userid if exists, else return unknown
+function getPremiumUserIdOrUnknown() {
+    var userId = getPremiumUserId();
+    return userId === "" ? "unknown" : userId;
+}
+
+function getCountryCode() {
+    var _geoOptions = [
+      (document.location.search.match(/(?:[?&])geo=([a-zA-Z0-9]+)/) || {})[1],
+      (typeof adblockGeo === "object" && typeof adblockGeo.countryCode === "string") ? adblockGeo.countryCode : "unknown",
+      "unknown"
+    ];
+    return _geoOptions.filter(function(o) { return o !== undefined; })[0];
+}
+
+function getLanguage() {
+    var lan = (document.location.search.match(/(?:[?&])lang=([a-zA-Z0-9_]+)/) || {})[1];
+    if (!lan) {
+        lan = getLanguageInPath();
+    }
+    if (!lan) {
+        lan = window.navigator.userLanguage || window.navigator.language || "";
+    }
+    return lan;
+}
+
+function getLanguageQueryString() {
+    var lan = (document.location.search.match(/(?:[?&])lang=([a-zA-Z0-9_]+)/) || {})[1]
+    if (lan === undefined) {
+        return "";
+    }
+    return lan;
+}
+
+function getLanguageInPath() {
+    const firstPath = window.location.pathname.split('/')[1];
+    try {
+        Intl.getCanonicalLocales(firstPath.replace("_", "-"));
+    } catch (error) {
+        return ""
+    }
+    return firstPath;
+}
+
+function getTwoLetterLocale() {
+    var lan = getLanguage();
+    return lan.slice(0, 2);
+}
+
+function getFirstRunBool() {
+    var innerText = (document.getElementById('adblock_first_run_id') || {}).innerText;
+    if (innerText === undefined) {
+        return undefined;
+    }
+    return innerText === 'true';
+}
+
+// Returns "SI" if /installed, "SY" if /pay, "SG" if /, and "SU" for unknown
+function getSource() {
+    return "S" + getPlainSource();
+}
+
+function getPlainSource() {
+    var a = "U";
+    if (location.pathname.indexOf('premium') == -1) {
+        if (location.pathname.indexOf('mobile/installed') != -1)
+            a = "MI"
+        else if (location.pathname.indexOf('install') != -1)
+            a = "I";
+        else if (location.pathname.indexOf('mobile/pay') != -1)
+            a = "M"
+        else if (location.pathname.indexOf('mobile/test') != -1)
+            a = "T"
+        else if (location.pathname.indexOf('pay') != -1)
+            a = "Y"
+        else if (location.pathname.length == 1)
+            a = "G";
+        else if (
+            location.pathname.length > 1
+            && getLocalesIndex().includes(location.pathname.split("/")[1])
+            && location.pathname.split("/").length === 3
+        )
+            a = "G";
+        else if (location.pathname.indexOf('survey') != -1)
+            a = "Q";
+        else if (location.pathname.indexOf('update') != -1)
+            a = "B";
+        else if (location.pathname.indexOf('chrome') != -1)
+            a = "GC";
+        else if (location.pathname.indexOf('edge') != -1)
+            a = "GE";
+        else if (location.pathname.indexOf('firefox') != -1)
+            a = "GF";
+        else if (location.pathname.indexOf('safari') != -1)
+            a = "GS";
+        else if (location.pathname.indexOf('iOS') != -1)
+            a = "GI";
+        else if (location.pathname.indexOf('android') != -1)
+            a = "GA";
+        else if (location.pathname.indexOf('thanks') != -1)
+            a = "GT";
+        else if (location.pathname.indexOf('block-ads-and-popups') != -1)
+            a = "BAAP";
+        else if (location.pathname.indexOf('block-facebook-ads-with-adblock') != -1)
+            a = "BFBA";
+        else if (location.pathname.indexOf('block-youtube-ads-with-adblock') != -1)
+            a = "BYTA";
+        else if (location.pathname.indexOf('block-twitch-ads-with-adblock') != -1)
+            a = "BTA";
+        else if (location.pathname.indexOf('cryptocurrency-mining') != -1)
+            a = "CM";
+        else if (location.pathname.indexOf('malware-protection') != -1)
+            a = "MP";
+    } else {
+        // Must be before otherwise returns 'ME'
+        if (location.pathname.indexOf('premium/enrollment/distraction-control')  != -1) {
+            a = "MEDC";
+        } else if (location.pathname.indexOf('premium/enrollment') != -1) {
+            a = "ME";
+        } else if (location.pathname.indexOf('premium') != -1) {
+            a = "HME";
+        } else if (location.pathname.indexOf('installed') != -1) {
+            a = "Z";
+        } else if (location.pathname.indexOf('payment') != -1) {
+            a = "X";
+        } else if (location.pathname.indexOf('terms') != -1) {
+            a = "PS";
+        } else if (location.pathname.indexOf('privacy') != -1) {
+            a = "PP";
+        } else if (location.pathname.indexOf('thankyou') != -1) {
+            a = "PT";
+        } else if (location.pathname.indexOf('uninstall') != -1) {
+            a = "PU";
+        }
+    }
+    if ((location.pathname.indexOf('/update/4.6.0/1') != -1) ||
+        (location.pathname.indexOf('/update/4.6.0/2') != -1) ||
+        (location.pathname.indexOf('/update/4.6.0/3') != -1) ||
+        (location.pathname.indexOf('/update/4.7.3/1') != -1) ||
+        (location.pathname.indexOf('/update/4.7.3/2') != -1)) {
+        // TODO: remove me after the current /update/4.6.0 and / update/4.7.3 experiments.
+        a = "ME";
+    }
+    return a
+}
+
+function isProd() {
+    if (document.location.href.indexOf('localhost') == -1 &&
+        document.location.href.indexOf('dev.getadblock') == -1) {
+        return true;
+    }
+    return false;
+}
+
+function isEnglish() {
+    var lan = window.navigator.userLanguage || window.navigator.language;
+    var nonEnglish = (lan.slice(0, 2) !== "en");
+    if (nonEnglish) {
+        return false;
+    }
+    return true;
+}
+
+function isIOS() {
+    if (navigator.userAgent.indexOf("iPhone") != -1 ||
+        navigator.userAgent.indexOf("iPad") != -1 ||
+        navigator.userAgent.indexOf("iPod") != -1) {
+        return true;
+    }
+    return false;
+}
+
+function getOSSingleChar() {
+    var a = "U";
+    if (- 1 != navigator.appVersion.indexOf("Win")) {
+        a = "W";
+    } else if (isIOS()) {
+        a = "I";
+    } else if (- 1 != navigator.appVersion.indexOf("Mac")) {
+        a = "M";
+    } else if (- 1 != navigator.appVersion.indexOf("X11")) {
+        a = "L";
+    } else if (- 1 != navigator.appVersion.indexOf("Linux")) {
+        a = "L";
+    }
+    return a
+
+}
+
+// Returns OS from navigator object
+function getOS() {
+    return "O" + getOSSingleChar();
+}
+
+function getOSVersion() {
+    var forcedOSVersion = (document.location.search.match(/(?:[?&])ov=([0-9_]+)/) || {})[1];
+    if (typeof forcedOSVersion !== "undefined") {
+        return forcedOSVersion;
+    }
+    var match = navigator.userAgent.match(/(CrOS\ \w+|Windows\ NT|Mac\ OS\ X|Linux)\ ([\d\._]+)?/);
+    return (match || [])[2] || "unknown";
+}
+
+function getBrowser() {
+    var a = "U";
+    var forcedBrowser = (document.location.search.match(/(?:[?&])bro=([A-Z])/) || {})[1]
+    if (forcedBrowser !== undefined) {
+        return forcedBrowser;
+    }
+
+    var chrome = navigator.userAgent.indexOf("Chrome");
+    var opera = navigator.userAgent.indexOf("OPR");
+    var edg = navigator.userAgent.indexOf("Edg");
+    var edge = navigator.userAgent.indexOf("Edge");
+    var safari = navigator.userAgent.indexOf("Safari");
+    var firefox = navigator.userAgent.indexOf("Firefox");
+    var samsung = navigator.userAgent.indexOf("Samsung");
+    var trident = navigator.userAgent.indexOf("Trident");
+    if ((chrome !== -1) &&
+        (opera === -1) &&
+        (samsung === -1) &&
+        (edg === -1) &&
+        (edge === -1)) {
+        a = "E";
+    } else if ((safari !== -1) &&
+               (opera === -1) &&
+               (samsung === -1) &&
+               (edg === -1) &&
+               (edge === -1)) {
+        a = "S";
+    } else if (firefox !== -1) {
+        a = "F";
+    } else if (opera !== -1) {
+        a = "O";
+    } else if (edge !== -1) {
+        a = "M";
+    } else if (edg !== -1) {
+        a = "CM";
+    } else if (navigator.appName === 'Microsoft Internet Explorer') {
+        a = "T";
+    } else if (trident !== -1) {
+        a = "T";
+    } else if (samsung !== -1) {
+        a = "G";
+    }
+    return a;
+}
+
+// Builds tracking information
+// TODO name this something better
+function recordTracking() {
+    var x = "0";
+    var g = "0";
+    if (typeof _experiment !== 'undefined' && _experiment.isExperimentRunning("*")) {
+      x = _experiment.xNumber("*");
+      g = _experiment.variantIndex("*") + 1;
+    }
+    var a = getUserId(),
+        a = a == "" ? "NA" : a; // set NA if userid isn't set
+    if (typeof getPlainSource === "function" && (getPlainSource().startsWith("ME") || getPlainSource().startsWith("HME"))) {
+        // prefix MyAdBlock product tag if it's not core.
+        return "ME X" + x + "G" + g + " F" + getBrowser() + getOS() + getSource() + " " + a;
+    }
+    const getPremiumReward = JSON.parse(localStorage.getItem("get_premium_reward"));
+    if (getPremiumReward === true) {
+        // prefix MyAdBlock product tag if it's not core.
+        return "ME X" + x + "G" + g + " F" + getBrowser() + getOS() + getSource() + " " + a;
+    }
+    return "X" + x + "G" + g + " F" + getBrowser() + getOS() + getSource() + " " + a // return tracking string
+};
+
+function getGAID() {
+    if (typeof ga !== 'undefined' && typeof ga.getByName === 'function') {
+        var tracker = ga.getByName('gatracker');
+        if (tracker !== null) {
+            return tracker.get('clientId');
+        }
+        return '';
+    }
+    return '';
+};
+
+function isPremium() {
+    var abPremium = !!document.location.pathname.match(/premium/);
+    return abPremium;
+}
+
+function getPremiumCid() {
+    if (isPremium()) {
+        if (typeof premiumVars === 'object' && typeof premiumVars.cid === "string") {
+            return premiumVars.cid;
+        }
+    }
+    return "0";
+}
+
+function getPremiumSid() {
+    if (isPremium()) {
+        if (typeof premiumVars === 'object' && typeof premiumVars.sid === "string") {
+            return premiumVars.sid;
+        }
+    }
+    return "0";
+}
+
+function getPremiumSession() {
+    if (isPremium()) {
+        if (typeof premiumVars === 'object' && typeof premiumVars.sess === "string") {
+            return premiumVars.sess;
+        }
+    }
+    return "";
+}
+
+function getPurchaseMetadata(flavor, testMode) {
+    var string = "";
+    if (typeof _experiment !== 'undefined') {
+        string = "E="+_experiment.name(flavor)+"EI="+_experiment.experimentId(flavor)+"V="+_experiment.variant(flavor)+
+                  "VI="+_experiment.variantIndex(flavor);
+    } else {
+        string = "E=EI=V=VI=";
+    }
+
+    if (typeof getUserId === 'function') {
+        string = string + "U="+getUserId();
+    } else {
+        string = string + "U=";
+    }
+
+    string = string + "G=" + getGAID();
+    string = string + "L=" + getLanguage();
+    string = string + "C=" + getCountryCode();
+    string = string + "P=" + (typeof isPremium === "function" ? (isPremium() ? "true" : "false") : "false");
+    string = string + "CID=" + (typeof getPremiumCid === "function" ? getPremiumCid() : "0");
+    string = string + "SID=" + (typeof getPremiumSid === "function" ? getPremiumSid() : "0");
+    return string;
+}
+
+// Should be used to run a function that expects the userId to
+// be present.  This functionality is useful since the userId is
+// not present until window.onload.
+//
+// To register a function to run call _userIdDispatch.runWithUserId(func)
+// with the desired function.  The user ID will be supplied as the only
+// parameter.  The user ID can be an empty string.
+//
+// ** Call runWithUserId before window.onload
+var _userIdDispatch = (function() {
+    var callbacks = [];
+
+    var runCallbacks = function() {
+        var numTrys = 0;
+        (function checkForInjection() {
+            // Run later to get behind the userid injection.
+            setTimeout(function() {
+                var userId = getUserIdOrUnknown();
+                var firstRun = getFirstRunBool();
+                var dispatchNow = false;
+                if (userId === "unknown" || firstRun === undefined) {
+                    numTrys++;
+                    if (numTrys <= 10) {
+                        checkForInjection();
+                    } else {
+                        dispatchNow = true;
+                    }
+                } else {
+                    dispatchNow = true;
+                }
+
+                if (dispatchNow) {
+                    for (var i = 0; i < callbacks.length; i++) {
+                        callbacks[i](userId, firstRun);
+                    }
+                }
+            }, 1000);
+        })();
+    };
+    window.addEventListener('load', runCallbacks, false);
+
+    return {
+        runWithUserId: function(func) {
+            callbacks.push(func);
+        }
+    }
+})();
