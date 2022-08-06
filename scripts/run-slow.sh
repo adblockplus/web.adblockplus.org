341dfbe1b9404279062ded2bb5c6a1bac4e179b9
Laura Barluzzi <laura@getadblock.com>
2022-08-06 09:58:01 +0200

    Add logs

--- scripts/run-slow.sh
+++ scripts/run-slow.sh
@@ -1,3 +1,7 @@
+echo ">>> build"
 npm run min
+echo ">>> generate static pages"
 python2 -m cms.bin.generate_static_pages . public
+echo ">>> start apache"
 service apache2 start
+echo ">>> all done"
