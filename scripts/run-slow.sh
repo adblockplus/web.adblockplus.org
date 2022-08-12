9cc7ffdd47575b5fe4f449ab53b8a773d97ab6ab
Laura Barluzzi <laura@getadblock.com>
2022-08-12 11:32:19 +0200

    Remove logs for debugging slow build

--- scripts/run-slow.sh
+++ scripts/run-slow.sh
@@ -1,7 +1,3 @@
-echo ">>> build"
 npm run min
-echo ">>> generate static pages"
 python3 -m cms.bin.generate_static_pages . public
-echo ">>> start apache"
 service apache2 start
-echo ">>> all done"
