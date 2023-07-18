706b5a7f11c6562e9f7d894036ef551ba7c03fc9
Julian Doucette <j.doucette@adblock.org>
2023-07-18 15:04:15 +0100

    Revert "fix: Disabled monthly/yearly selection in premium page plan ctas because it won't work for paddle checkout mvp"
    This reverts commit e4df9245f24e19f1d724d8fd00d3463d8ba4add9.

--- static/js/pages/premium.js
+++ static/js/pages/premium.js
@@ -50,7 +50,6 @@ function setAutoHeights(targets)
  }
 
 // Enable pre-selecting monthly/yearly payment options via clicking a 
-<<<<<<< Updated upstream
 // .premium-button[data-plan] with an allowlisted plan
 const premiumPlans = ['monthly', 'yearly'];
 window.addEventListener('click', event => {
@@ -67,24 +66,6 @@ window.addEventListener('click', event => {
       .click();
   }
 });
-=======
-// .premium-cta[data-plan] with an allowlisted plan
-// const premiumPlans = ['monthly', 'yearly'];
-// window.addEventListener('click', event => {
-//   if (
-//     event.target.classList
-//     && event.target.classList.contains('premium-cta')
-//     && event.target.dataset
-//     && event.target.dataset.plan
-//   ) {
-//     const plan = event.target.dataset.plan;
-//     if (premiumPlans.indexOf(plan) == -1) return;
-//     document
-//       .querySelector(`[data-recurring-frequency="${plan}"]`)
-//       .click();
-//   }
-// });
->>>>>>> Stashed changes
 
 function onDOMContentLoaded()
 {
