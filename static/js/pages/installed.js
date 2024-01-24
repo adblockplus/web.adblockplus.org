38b163fbf37624b36884f7af0572954ed29202e3
Laura Barluzzi <laura@getadblock.com>
2024-01-24 20:03:13 +0100

    Add /installed page section only for Firefox users and supported ext versions

--- static/js/pages/installed.js
+++ static/js/pages/installed.js
@@ -2,10 +2,21 @@
 
 var SCROLL_TICK_LENGTH = 10;
 var SCROLL_TIME = 500;
+var MIN_FX_VERSION = 3.22;
 
 var page = document.scrollingElement || document.documentElement; // IE
 var body = document.body;
 var donationHeading = document.querySelector('.payment-heading');
+var dataCollectionParagraph = document.getElementById('fx-data-collection');
+
+var installedParams = new URLSearchParams(window.location.search);
+var browser = installedParams.get('ap');
+if (browser === "firefox") {
+  const versionNumber = parseFloat(installedParams.get('av'));
+  if (!isNaN(versionNumber) && versionNumber >= MIN_FX_VERSION) {
+    dataCollectionParagraph.style.display = 'block';
+  }
+}
 
 window.addEventListener('resize', function() {
   if (window.innerWidth > 991)
