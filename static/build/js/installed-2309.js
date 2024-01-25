b542a8399f40fe699a48545a23881e75913d93c6
Laura Barluzzi <laura@getadblock.com>
2024-01-25 12:06:48 +0100

    Update static build assets

--- static/build/js/installed-2309.js
+++ static/build/js/installed-2309.js
@@ -1541,23 +1541,27 @@ if (currentBrowser.toLowerCase() === "firefox") {
   // 3.22 introduced the content script logic to listen to the hyperlink click
   // 3.21.1 is the latest version without that support
   const shouldShowDataCollectionText = (version) => {
-    const versionArray = version.split('.').map((number) => parseInt(number));
+    const thisVersion = version.split('.').map((number) => parseInt(number));
     const lastUnsupported = [ 3, 21, 1 ];
 
-    for (var i in versionArray) {
-      if (versionArray[i] > lastUnsupported[i]) {
+    if (!version) {
+      return false;
+    }
+
+    for (var i in thisVersion) {
+      if (thisVersion[i] > lastUnsupported[i]) {
         return true;
       }
-      else if (versionArray[i] < lastUnsupported[i]) {
+      else if (thisVersion[i] < lastUnsupported[i]) {
         return false;
       }
-      else if (versionArray.length - 1 == i) {
+      else if (thisVersion.length - 1 == i) {
         return false;
       }
     }
   };
 
-  if (version && shouldShowDataCollectionText(version)) {
+  if (shouldShowDataCollectionText(version)) {
     document.getElementById('fx-data-collection').style.display = 'block';
   }
 }
