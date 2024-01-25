6ea07a4a26263147de394bb966d6224710bbf766
Laura Barluzzi <laura@getadblock.com>
2024-01-25 09:02:20 +0000

    Remove parsing to float

--- static/build/js/installed.js
+++ static/build/js/installed.js
@@ -1535,11 +1535,18 @@ const installedParams = new URLSearchParams(window.location.search);
 const currentBrowser = installedParams.get('ap');
 
 if (currentBrowser === "firefox") {
-  const minSupportedVersion = 3.22;
-  const dataCollectionParagraph = document.getElementById('fx-data-collection');
-  const versionNumber = parseFloat(installedParams.get('av'));
+  const version = parseFloat(installedParams.get('av'));
 
-  if (!isNaN(versionNumber) && versionNumber >= minSupportedVersion) {
-    dataCollectionParagraph.style.display = 'block';
+  const showDataCollectionText = (v1, v2) => {
+    const thisVersion = v1.split(".").map((number) => parseInt(number));
+    const lastUnsupported = v2.split(".").map((number) => parseInt(number));
+    return thisVersion.some((num, i) => num > lastUnsupported[i]);
+  };
+
+  // List of release versions here: https://blog.adblockplus.org/releases
+  // 3.22 introduced the content script logic to listen to the hyperlink click
+  // 3.21.1 is the latest version without that support
+  if (version && showDataCollectionText(version, "3.21.1")) {
+    document.getElementById('fx-data-collection').style.display = 'block';
   }
 }
