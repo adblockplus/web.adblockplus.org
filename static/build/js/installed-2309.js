e5d71c74d99e015773dd9b2ff16f26a7b356bd22
Laura Barluzzi <laura@getadblock.com>
2024-01-25 11:21:21 +0100

    Update static build assets

--- static/build/js/installed-2309.js
+++ static/build/js/installed-2309.js
@@ -1537,16 +1537,27 @@ const currentBrowser = installedParams.get('ap');
 if (currentBrowser.toLowerCase() === "firefox") {
   const version = installedParams.get('av');
 
-  const shouldShowDataCollectionText = (v1, v2) => {
-    const thisVersion = v1.split(".").map((number) => parseInt(number));
-    const lastUnsupported = v2.split(".").map((number) => parseInt(number));
-    return thisVersion.some((num, i) => num > lastUnsupported[i]);
-  };
-
   // List of release versions here: https://blog.adblockplus.org/releases
   // 3.22 introduced the content script logic to listen to the hyperlink click
   // 3.21.1 is the latest version without that support
-  if (version && shouldShowDataCollectionText(version, "3.21.1")) {
+  const shouldShowDataCollectionText = (version) => {
+    const versionArray = version.split('.').map((number) => parseInt(number));
+    const lastUnsupported = [ 3, 21, 1 ];
+
+    for (var i in versionArray) {
+      if (versionArray[i] > lastUnsupported[i]) {
+        return true;
+      }
+      else if (versionArray[i] < lastUnsupported[i]) {
+        return false;
+      }
+      else if (versionArray.length - 1 == i) {
+        return false;
+      }
+    }
+  };
+
+  if (version && shouldShowDataCollectionText(version)) {
     document.getElementById('fx-data-collection').style.display = 'block';
   }
 }
