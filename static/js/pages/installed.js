606f587b0d410e20a1e282fc772a09a29fb41347
Laura Barluzzi <laura@getadblock.com>
2024-01-25 10:26:03 +0100

    Use a more accurate function name

--- static/js/pages/installed.js
+++ static/js/pages/installed.js
@@ -6,7 +6,7 @@ const currentBrowser = installedParams.get('ap');
 if (currentBrowser.toLowerCase() === "firefox") {
   const version = installedParams.get('av');
 
-  const showDataCollectionText = (v1, v2) => {
+  const shouldShowDataCollectionText = (v1, v2) => {
     const thisVersion = v1.split(".").map((number) => parseInt(number));
     const lastUnsupported = v2.split(".").map((number) => parseInt(number));
     return thisVersion.some((num, i) => num > lastUnsupported[i]);
@@ -15,7 +15,7 @@ if (currentBrowser.toLowerCase() === "firefox") {
   // List of release versions here: https://blog.adblockplus.org/releases
   // 3.22 introduced the content script logic to listen to the hyperlink click
   // 3.21.1 is the latest version without that support
-  if (version && showDataCollectionText(version, "3.21.1")) {
+  if (version && shouldShowDataCollectionText(version, "3.21.1")) {
     document.getElementById('fx-data-collection').style.display = 'block';
   }
 }
