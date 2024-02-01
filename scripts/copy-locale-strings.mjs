3bb51d37ff1ba770d0d1df97b732784ba26c3be9
julian <julian@adblock.org>
2024-02-01 18:25:06 +0000

    feat: Implemented block-cookie-banners-v2 page

---
+++ scripts/copy-locale-strings.mjs
@@ -0,0 +1,40 @@
+import { program } from "commander";
+import { readFileSync, readdirSync, writeFileSync } from "fs";
+import { join } from "path";
+
+program
+  .description("copy strings from one locale file to another")
+  .requiredOption("-i, --input <name>", "input locale file name (without .json)")
+  .requiredOption("-o, --output <name>", "output locale file name (without .json)")
+  .requiredOption("-s, --strings <ids>", "comma separated string ids to copy")
+
+program.parse();
+const options = program.opts();
+const inputFilename = `${options.input}.json`;
+const outputFilename = `${options.output}.json`;
+const strings = options.strings.split(",");
+const locales = readdirSync("locales");
+
+const hits = [];
+const misses = [];
+
+for (const locale of locales) {
+  let hit = false;
+  try {
+    const inputPath = join("locales", locale, inputFilename);
+    const outputPath = join("locales", locale, outputFilename);
+    const input = JSON.parse(readFileSync(inputPath));
+    const output = JSON.parse(readFileSync(outputPath));
+    for (const string of strings) {
+      if (input[string]) {
+        output[string] = input[string];
+        hit = true;  
+      }
+    }
+    if (hit) writeFileSync(outputPath, JSON.stringify(output, null, 2));
+  } catch (err) { /* silence is golden */ }
+  if (hit) hits.push(locale);  
+  else misses.push(locale);
+}
+
+console.log({ hits, misses });
