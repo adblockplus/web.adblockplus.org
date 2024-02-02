2905a3e8ae0b409336a1d4dc072810f366bd9e0c
Laura Barluzzi <laura@getadblock.com>
2024-02-02 08:30:22 +0000

    Minimize query params for handoff

--- static/adblock/anti-adblock-pass/payment.js
+++ static/adblock/anti-adblock-pass/payment.js
@@ -491,13 +491,13 @@ $(document).ready(function() {
         const clickTimestamp = Date.now();
         const [amount, frequency] = getSelectedAmountFrequencyPlan();
         queryParams.set("premium-checkout__handoff", 1);
-        queryParams.set("premium-checkout__flow", "anti-adblock-pass");
         queryParams.set("premium-checkout__userid", getUserId());
-        queryParams.set("premium-checkout__currency", "USD");
-        queryParams.set("premium-checkout__amount", amount * 100);
-        queryParams.set("premium-checkout__frequency", frequency);
-        queryParams.set("premium-checkout__language", language);
-        queryParams.set("premium-checkout__timestamp", clickTimestamp);  
+        // queryParams.set("premium-checkout__currency", "USD");
+        // queryParams.set("premium-checkout__amount", amount * 100);
+        // queryParams.set("premium-checkout__frequency", frequency);
+        // queryParams.set("premium-checkout__language", language);
+       //  queryParams.set("premium-checkout__timestamp", clickTimestamp);  
+
         return `https://accounts.adblockplus.org/${language}/premium?${queryParams.toString()}`;
     }
 
