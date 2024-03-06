9a36faba241c79ded535b2ae6aa3176b444a86d3
julian <julian@adblock.org>
2024-03-06 11:09:01 +0000

    fix: Removed outdated failing tests

--- tests/web.adblockplus.org/tests/test_CookiesSettings.py
+++ tests/web.adblockplus.org/tests/test_CookiesSettings.py
@@ -49,8 +49,6 @@ def test_verify_all_options_selected_by_default(driver):
     cookie_settings_form = cookies_prompt.click_edit_cookie_settings_button()
     assert cookie_settings_form.is_necessary_cookies_toggle_enabled()
     assert cookie_settings_form.is_tracking_cookies_toggle_enabled()
-    assert cookie_settings_form.is_ab_testing_cookies_toggle_enabled()
-
 
 def test_disable_tracking_save(driver):
     landing_page = LandingPage(driver)
@@ -62,41 +60,3 @@ def test_disable_tracking_save(driver):
     cookie_settings_form.click_save_preferences_button()
 
     assert not cookies_prompt.is_prompt_visible()
-
-
-def test_disable_ab_testing_save(driver):
-    landing_page = LandingPage(driver)
-    landing_page.go_home()
-
-    cookies_prompt = CookiesPrompt(driver)
-    cookie_settings_form = cookies_prompt.click_edit_cookie_settings_button()
-    cookie_settings_form.click_ab_testing_cookies_toggle()
-    cookie_settings_form.click_save_preferences_button()
-
-    assert not cookies_prompt.is_prompt_visible()
-
-
-def test_verify_tracking_toggle_affects_ab_when_ab_enabled(driver):
-    landing_page = LandingPage(driver)
-    landing_page.go_home()
-
-    cookies_prompt = CookiesPrompt(driver)
-    cookie_settings_form = cookies_prompt.click_edit_cookie_settings_button()
-    cookie_settings_form.click_tracking_cookies_toggle()
-    assert not cookie_settings_form.is_ab_testing_cookies_toggle_enabled()
-    cookie_settings_form.click_tracking_cookies_toggle()
-    assert cookie_settings_form.is_ab_testing_cookies_toggle_enabled()
-
-
-def test_verify_tracking_toggle_does_not_affect_ab_when_ab_disabled(driver):
-    landing_page = LandingPage(driver)
-    landing_page.go_home()
-
-    cookies_prompt = CookiesPrompt(driver)
-    cookie_settings_form = cookies_prompt.click_edit_cookie_settings_button()
-    cookie_settings_form.click_ab_testing_cookies_toggle()
-    cookie_settings_form.click_tracking_cookies_toggle()
-    assert not cookie_settings_form.is_ab_testing_cookies_toggle_enabled()
-    cookie_settings_form.click_tracking_cookies_toggle()
-    assert not cookie_settings_form.is_ab_testing_cookies_toggle_enabled()
-
