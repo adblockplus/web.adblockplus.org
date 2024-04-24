d7635c74008f2a02c79539dadd8cd69a11d089cc
julian <julian@adblock.org>
2024-04-24 15:32:46 +0100

    feat: Added all contribution currencies to premium page flows

--- static/premium/checkout.js
+++ static/premium/checkout.js
@@ -31,6 +31,86 @@ const PADDLE = adblock.config.paddle = {
           amount: "4000",
           product: 68394,
         },
+      },
+      "CAD": {
+        "monthly": {
+          amount: "400",
+          product: 68592,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 68593,
+        },
+      },
+      "GBP": {
+        "monthly": {
+          amount: "400",
+          product: 68594,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 68595,
+        },
+      },
+      "AUD": {
+        "monthly": {
+          amount: "400",
+          product: 68590,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 68591,
+        },
+      },
+      "NZD": {
+        "monthly": {
+          amount: "400",
+          product: 68596,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 68597,
+        },
+      },
+      "CHF": {
+        "monthly": {
+          amount: "400",
+          product: 68598,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 68599,
+        },
+      },
+      "PLN": {
+        "monthly": {
+          amount: "1500",
+          product: 68604,
+        },
+        "yearly": {
+          amount: "15000",
+          product: 68605,
+        },
+      },
+      "JPY": {
+        "monthly": {
+          amount: "600",
+          product: 68600,
+        },
+        "yearly": {
+          amount: "6000",
+          product: 68601,
+        },
+      },
+      "RUB": {
+        "monthly": {
+          amount: "30000",
+          product: 68602,
+        },
+        "yearly": {
+          amount: "300000",
+          product: 68603,
+        },
       }
     },
     LIVE: {
@@ -53,6 +133,86 @@ const PADDLE = adblock.config.paddle = {
           amount: "4000",
           product: 877579,
         }
+      },
+      "CAD": {
+        "monthly": {
+          amount: "400",
+          product: 879254,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 879263,
+        },
+      },
+      "GBP": {
+        "monthly": {
+          amount: "400",
+          product: 879255,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 879264,
+        },
+      },
+      "AUD": {
+        "monthly": {
+          amount: "400",
+          product: 879256,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 879265,
+        },
+      },
+      "NZD": {
+        "monthly": {
+          amount: "400",
+          product: 879257,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 879266,
+        },
+      },
+      "CHF": {
+        "monthly": {
+          amount: "400",
+          product: 879258,
+        },
+        "yearly": {
+          amount: "4000",
+          product: 879267,
+        },
+      },
+      "PLN": {
+        "monthly": {
+          amount: "1500",
+          product: 879259,
+        },
+        "yearly": {
+          amount: "15000",
+          product: 879268,
+        },
+      },
+      "JPY": {
+        "monthly": {
+          amount: "600",
+          product: 879260,
+        },
+        "yearly": {
+          amount: "6000",
+          product: 879269,
+        },
+      },
+      "RUB": {
+        "monthly": {
+          amount: "30000",
+          product: 879261,
+        },
+        "yearly": {
+          amount: "300000",
+          product: 879270,
+        },
       }
     }
   },
