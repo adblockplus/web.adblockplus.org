aa009055313ddc1898d8b93c8c7c69360e7940b5
julian <julian@adblock.org>
2024-07-15 19:34:32 +0000

    feat: Implemented shared checkout function and checkout test page

--- static/shared/currency.js
+++ static/shared/currency.js
@@ -1,24 +1,32 @@
+
+export function getNumber(string) {
+  return getAccountingNumber(parseFloat(string.replace(/\D/g, "").trim()));
+}
+
+export function getAccountingNumber(number) {
+  return parseFloat(parseFloat(number).toFixed(2));
+}
+
 export function getDollarNumber(currency, centAmountString) {
   const centAmountNumber = parseInt(centAmountString, 10);
-  return currency == "JPY" ? centAmountNumber : centAmountNumber / 100;
+  return currency == "JPY" ? centAmountNumber : getAccountingNumber(centAmountNumber / 100);
 }
 
 export function getCentNumber(currency, dollarString) {
-  const dollarNumber = parseFloat(dollarString);
-  return currency == "JPY" ? dollarNumber : dollarNumber * 100;
+  const dollarNumber = getAccountingNumber(dollarString);
+  return currency == "JPY" ? dollarNumber : getAccountingNumber(dollarNumber * 100);
 }
 
-export function getDollarString(currency, centAmountString) {
+export function getDollarString(currency, centAmountString, showTrailingZeros = false) {
   const dollarNumber = getDollarNumber(currency, centAmountString);
   const formatOptions = {
-    style: 'currency', 
-    currency: currency, 
+    style: 'currency',
+    currency: currency,
     currencyDisplay: 'narrowSymbol'
   };
-  if (dollarNumber % 1 === 0) {
+  if (!showTrailingZeros && dollarNumber % 1 == 0) {
     formatOptions.minimumFractionDigits = 0;
     formatOptions.maximumFractionDigits = 0;
   }
-  const language = String(document.documentElement.lang) || "en";
-  return new Intl.NumberFormat(language.replace("_", "-"), formatOptions).format(dollarNumber);
+  return new Intl.NumberFormat(adblock.settings.locale.replace("_", "-"), formatOptions).format(dollarNumber);
 }
