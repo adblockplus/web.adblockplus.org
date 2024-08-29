fd052e2ad461207df6ae0343960086267c313e62
julian <julian@adblock.org>
2024-08-29 10:33:45 +0100

    feat: Implemented contribution style installed page and removed obsolete installed-2309 page and AppealForm component

--- static/installed/installed.js
+++ static/installed/installed.js
@@ -1,127 +1,172 @@
-/* global Paddle, adblock */
-import { getDollarString, getDollarNumber, getCentNumber } from "../shared/currency.js";
-import { PaymentOptions, getInstalledAmount, getInstalledMinimum } from "./PaymentOptions.js";
-import { checkout } from "../shared/checkout.js";
+import "/js/vendor/NumberFormat.min.js";
+import { getDollarNumber, getDollarString } from "../shared/currency.js";
+import { checkout } from "../shared/checkout.js"
 
-const LOCALE_BREAKPOINT = 8;
-const CURRENCY_BREAKPOINT = 5;
+const PAYMENT_CONFIG = {
+  USD: {
+    once: [1000,1500,2000,3500,5000,],
+    monthly: [199,299,399,499,999,],
+    yearly: [1000,1500,2000,3500,5000,],
+  },
+  AUD: {
+    once: [1000,1500,2000,3500,5000,],
+    monthly: [199,299,399,499,999,],
+    yearly: [1000,1500,2000,3500,5000,],
+  },
+  CAD: {
+    once: [1000,1500,2000,3500,5000,],
+    monthly: [199,299,399,499,999,],
+    yearly: [1000,1500,2000,3500,5000,],
+  },
+  EUR: {
+    once: [1000,1500,2000,3500,5000,],
+    monthly: [199,299,399,499,999,],
+    yearly: [1000,1500,2000,3500,5000,],
+  },
+  GBP: {
+    once: [1000,1500,2000,3500,5000,],
+    monthly: [199,299,399,499,999,],
+    yearly: [1000,1500,2000,3500,5000,],
+  },
+  JPY: {
+    once: [1500,2000,2500,3500,5000,],
+    monthly: [200,300,500,1000,1500,],
+    yearly: [1500,2000,2500,3500,5000,],
+  },
+  NZD: {
+    once: [1000,1500,2000,3500,5000,],
+    monthly: [199,299,399,499,999,],
+    yearly: [1000,1500,2000,3500,5000,],
+  },
+  CHF: {
+    once: [1000,1500,2000,3500,5000,],
+    monthly: [199,299,399,499,999,],
+    yearly: [1000,1500,2000,3500,5000,],
+  },
+  RUB: {
+    once: [25000,50000,100000,250000,500000,],
+    monthly: [15000,25000,40000,50000,100000,],
+    yearly: [25000,50000,100000,250000,500000,],
+  },
+  PLN: {
+    once: [3999,5999,7999,13999,19999,],
+    monthly: [799,1199,1599,1999,3999,],
+    yearly: [3999,5999,7999,13999,19999,],
+  },
+};
+
+const defaultCurrency = adblock.settings.defaultCurrency || "USD";
 
 const paymentForm = document.querySelector(".installed-payment");
-const currencySelect = document.querySelector(".installed-payment-currency__select");
-const optionButtons = document.querySelectorAll(".installed-payment-option");
-const monthlyFixedOptionButtons = document.querySelectorAll(".installed-payment-frequency--monthly .installed-payment-option--fixed");
-const yearlyFixedOptionButtons = document.querySelectorAll(".installed-payment-frequency--yearly .installed-payment-option--fixed");
-const monthlyCustomOptionButton = document.querySelector(".installed-payment-frequency--monthly .installed-payment-option--custom");
-const yearlyCustomOptionButton = document.querySelector(".installed-payment-frequency--yearly .installed-payment-option--custom");
-const rewardText = document.querySelector(".payment-reward__text");
-const checkoutButton = document.querySelector(".installed-payment-checkout__button");
+const paymentCurrency = paymentForm.querySelector(".installed-payment-header__select");
 
-// apply form presentation modifier if the translated placeholder text exceeds the length of the custom amount field
-if (yearlyCustomOptionButton.placeholder.length > LOCALE_BREAKPOINT) 
-  paymentForm.classList.add("installed-payment--long-locale");
-
-// populate currencies, select default currency, and reference the selected currency
-currencySelect.innerHTML = "";
-for (const currency in PaymentOptions) {
-  const option = document.createElement("option");
-  option.textContent = currency;
-  currencySelect.appendChild(option);
+function constructPaymentCurrencies() {
+  for (const currency in PAYMENT_CONFIG) {
+    if (currency == defaultCurrency) continue;
+    const option = document.createElement("option");
+    option.textContent = currency;
+    paymentCurrency.appendChild(option);
+  }
+  paymentCurrency.value = defaultCurrency;
 }
-let selectedCurrency = currencySelect.value = adblock.settings.defaultCurrency || "USD";
+constructPaymentCurrencies();
 
-let selectedOption = document.querySelector(".installed-payment-option--active");
+function getSelectedCurrency() {
+  return paymentCurrency.value;
+}
 
-function updateReward() {
-  const amount = selectedOption.dataset.amount // fixed amount
-    || getCentNumber(selectedCurrency, selectedOption.value); // custom amount
-  if (amount) { // don't update the reward when the custom amount is empty
-    rewardText.innerHTML = adblock.strings["payment-reward"].replace(
-      `<span>35.00</span>`, 
-      `<span class="amount">${getDollarString(selectedCurrency, amount)}</span>`
+function getSelectedFrequency(selected) {
+  return selected.closest(".installed-payment-frequency").dataset.frequency;
+}
+
+function getSelectedAmount(selected) {
+  return selected.value == "custom" 
+    ? selected.closest(".installed-payment-amount").querySelector(".installed-payment-amount__input").value
+    : selected.value;
+}
+
+function updatePaymentAmounts(currency) {
+  const paymentFrequencies = paymentForm.querySelectorAll(".installed-payment-frequency");
+  paymentFrequencies.forEach(paymentFrequency => {
+    const frequency = paymentFrequency.dataset.frequency;
+    const frequencyAmounts = paymentFrequency.querySelectorAll(".installed-payment-amount--fixed");
+    frequencyAmounts.forEach((amountLabel, amountIndex) => {
+      const amount = PAYMENT_CONFIG[currency][frequency][amountIndex];
+      amountLabel.querySelector(".installed-payment-amount__radio").value = amount;
+      amountLabel.querySelector(".installed-payment-amount__text").textContent = getDollarString(currency, amount);
+    });
+    paymentFrequency.querySelector(".installed-payment-amount__input").placeholder = getDollarString(
+      currency,
+      PAYMENT_CONFIG[currency][frequency][2]
     );
-  }
-}
-
-function getCurrencySign(string) {
-  return string.replace(/[\d,]/g, "").trim();
-}
-
-function handleCurrencyChange() {
-  let isLongCurrency = false;
-  selectedCurrency = currencySelect.value;
-  function updateFixedOptionButton(optionButton, index) {
-    const centAmountNumber = getInstalledAmount(selectedCurrency, optionButton.dataset.frequency, index);
-    const dollarAmountString = getDollarString(selectedCurrency, centAmountNumber);
-    optionButton.textContent = dollarAmountString;
-    optionButton.dataset.amount = centAmountNumber;
-    if (dollarAmountString.length > CURRENCY_BREAKPOINT) isLongCurrency = true;
-  }
-  monthlyFixedOptionButtons.forEach(updateFixedOptionButton);
-  yearlyFixedOptionButtons.forEach(updateFixedOptionButton);
-  function updateCustomOptionButton(optionButton) {
-    optionButton.dataset.min = getInstalledMinimum(selectedCurrency, optionButton.dataset.frequency);
-    // update custom option minimum if custom option is selected
-    if (optionButton.classList.contains("installed-payment-option--active")) {
-      optionButton.min = getDollarNumber(selectedCurrency, optionButton.dataset.min);
-    }
-  }
-  updateCustomOptionButton(yearlyCustomOptionButton);
-  updateCustomOptionButton(monthlyCustomOptionButton);
-  // apply form presentation modifier if the localised dollar amount text exceeds the length of the fixed option buttons
-  paymentForm.classList[isLongCurrency ? "add" : "remove"]("installed-payment--long-currency");
-  const customText = adblock.strings["installed-payment-option--custom"].replace("$", getCurrencySign(monthlyFixedOptionButtons[0].textContent));
-  yearlyCustomOptionButton.placeholder = customText;
-  monthlyCustomOptionButton.placeholder = customText;
-  updateReward();
-}
-currencySelect.addEventListener("change", handleCurrencyChange);
-handleCurrencyChange();
-
-function onOptionSelect(optionButton) {
-  selectedOption = optionButton;
-  const lastFrequency = document.querySelector(".installed-payment-frequency--active");
-  const nextFrequency = selectedOption.closest(".installed-payment-frequency");
-  if (nextFrequency != lastFrequency) {
-    lastFrequency.classList.remove("installed-payment-frequency--active");
-    nextFrequency.classList.add("installed-payment-frequency--active");
-  }
-  const lastOption = document.querySelector(".installed-payment-option--active");
-  if (selectedOption != lastOption) {
-    lastOption.classList.remove("installed-payment-option--active");
-    selectedOption.classList.add("installed-payment-option--active");
-  }
-  if (lastOption.classList.contains("installed-payment-option--custom")) {
-    lastOption.removeAttribute("min");
-    lastOption.required = false;
-  }
-  if (selectedOption.classList.contains("installed-payment-option--custom")) {
-    selectedOption.min = getDollarNumber(selectedCurrency, selectedOption.dataset.min);
-    selectedOption.required = true;
-  }
-  updateReward();
-}
-optionButtons.forEach(optionButton =>  optionButton.addEventListener("focus", () => onOptionSelect(optionButton)));
-yearlyCustomOptionButton.addEventListener("input", updateReward);
-monthlyCustomOptionButton.addEventListener("input", updateReward);
-
-const enable = element => { element.disabled = false; };  
-const disable = element => { element.disabled = true; };
-
-function onSubmit(event) {
-  event.preventDefault();
-  optionButtons.forEach(disable);
-  disable(checkoutButton);
-  checkout({
-    plan: "contribution",
-    currency: selectedCurrency,
-    frequency: selectedOption.dataset.frequency,
-    amount: selectedOption.dataset.amount || getCentNumber(selectedCurrency, selectedOption.value), // fixed || custom
-  })
-  .catch(() => adblock.error())
-  .finally(() => {
-    optionButtons.forEach(enable);
-    enable(checkoutButton);  
   });
 }
 
-paymentForm.addEventListener("submit", onSubmit);
+updatePaymentAmounts(defaultCurrency);
+
+paymentCurrency.addEventListener("change", () => updatePaymentAmounts(paymentCurrency.value));
+
+let requiredCustomAmount = null;
+
+paymentForm.addEventListener("focusin", event => {
+  // unset and untrack previously required custom amount on amount change
+  if (
+    (event.target.classList.contains("installed-payment-amount__radio")
+    || event.target.classList.contains("installed-payment-amount__input")) 
+    && requiredCustomAmount != null
+  ) {
+    requiredCustomAmount.required = null;
+    requiredCustomAmount.min = null;
+    requiredCustomAmount = null;
+  }
+  if (event.target.classList.contains("installed-payment-amount__input")) {
+    // check radios beside custom amounts when custom amounts are directly focused
+    event.target.closest(".installed-payment-amount").querySelector(".installed-payment-amount__radio").checked = true;
+    // set and track custom amount minimums when custom amounts are selected
+    requiredCustomAmount = event.target;
+    event.target.required = true;
+    event.target.min = getDollarNumber(
+      getSelectedCurrency(),
+      PAYMENT_CONFIG[getSelectedCurrency()][getSelectedFrequency(event.target)][0]
+    );
+  }
+  // focus custom amount inputs when custom amount radios are directly focused
+  if (event.target.classList.contains("installed-payment-amount__radio") && event.target.value == "custom") {
+    event.target.closest(".installed-payment-amount").querySelector(".installed-payment-amount__input").focus();
+  }
+});
+
+paymentForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const selected = paymentForm.querySelector(".installed-payment-amount__radio:checked");
+  const currency = getSelectedCurrency();
+  const frequency = getSelectedFrequency(selected)
+  const amount = getSelectedAmount(selected);
+  disablePaymentForm();
+  checkout({currency, frequency, amount})
+  .finally(enablePaymentForm);
+});
+
+function disablePaymentForm() {
+  paymentForm.classList.add("installed-payment--disabled");
+  paymentForm.querySelectorAll("input, button").forEach(field => { field.disabled = true; });
+}
+
+function enablePaymentForm() {
+  paymentForm.classList.remove("installed-payment--disabled");
+  paymentForm.querySelectorAll("input, button").forEach(field => { field.disabled = false; });
+}
+
+const applicationPlatform = adblock.query.get('ap');
+const applicationVersion = adblock.query.get('av');
+if (applicationPlatform == "firefox" && /^[0-9]+(\.[0-9])?(\.[0-9])?$/.test(applicationVersion)) {
+  const [currentMajor, currentMinor, currentPatch] = applicationVersion.split(".");
+  const [startingMajor, startingMinor, startingPatch] = "3.21.1".split(".");
+  if (
+    currentMajor > startingMajor
+    || currentMajor == startingMajor && currentMinor > startingMinor
+    || currentMajor == startingMajor && currentMinor == startingMinor && currentPatch >= startingPatch
+  ) {
+    document.getElementById('fx-data-collection').style.display = 'block';
+  }
+}
