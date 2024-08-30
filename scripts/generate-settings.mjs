d35840713476d1c2dacd6b86690edfb5a7b88d5f
julian <julian@adblock.org>
2024-08-30 11:09:38 +0100

    fix: Fixed regex context in settings DE_EXCEPTION

--- scripts/generate-settings.mjs
+++ scripts/generate-settings.mjs
@@ -14,11 +14,9 @@ const EUR_COUNTRIES = ["AT","BE","BG","CY","CZ","DE","DK","EE","ES","FI","FR","G
 
 const UNIQUE_CURRENCIES = {"AU":"AUD","CA":"CAD","CH":"CHF","GB":"GBP","JP":"JPY","NZ":"NZD","RU":"RUB"};
 
-const RE_LANGUAGE = /^[a-z]{2}[\-\_]{0,1}[A-Z]{0,2}$/
-
 const DE_EXCEPTION = `// EXCEPTION: Redirect all DE country to DE language when language is not specified; overriding browser preferred language.
 const pathSplit = location.pathname.split("/");
-const pathLanguage = pathSplit[1].test(RE_LANGUAGE) ? pathSplit[1] : null;
+const pathLanguage = pathSplit[1].test(/^[a-z]{2}[\-\_]{0,1}[A-Z]{0,2}$/) ? pathSplit[1] : null;
 const pageLanguage = document.documentElement.lang;
 if (pageLanguage != "de" && pathLanguage == null) location.href = location.origin + "/de" + location.pathname + location.search + location.hash;
 `;
