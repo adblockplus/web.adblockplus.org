88e8f5888dc7c639c523b9105a33088403cc810a
julian <julian@adblock.org>
2024-10-02 17:56:42 +0100

    feat: Implemented w291 prepared to refactor away contributionInfo

--- static/shared/checkout.js
+++ static/shared/checkout.js
@@ -969,30 +969,17 @@ export const checkout = adblock.api.checkout = function checkout(options) {
   let successUrl = PRODUCT_CONFIG[product].successUrl;
   if (typeof successUrl != "string" || !successUrl.length) throw new Error("Invalid successUrl");
 
-  try {
-    localStorage.setItem("contributionInfo", JSON.stringify({
-      currency,
-      frequency,
-      amount,
-      clickTs,
-      plan: productCode,
-      source: pageCode,
-      processor: "paddle",
-      lang: locale,
-      geo: country,
-    }));
-  } catch (error) {}
-
   const successParams = new URLSearchParams();
-  successParams.set("premium-checkout__handoff", 1);
-  successParams.set("premium-checkout__flow", flow);
-  successParams.set("premium-checkout__page", pageName);
-  successParams.set("premium-checkout__userid", premiumId);
-  successParams.set("premium-checkout__currency", currency);
-  successParams.set("premium-checkout__frequency", frequency);
-  successParams.set("premium-checkout__amount", amount);
-  successParams.set("premium-checkout__locale", locale);
-  successParams.set("premium-checkout__timestamp", clickTs);
+  successParams.set("checkout__flow", flow);
+  successParams.set("checkout__page", pageName);
+  successParams.set("checkout__product", product);
+  successParams.set("checkout__premiumId", premiumId);
+  successParams.set("checkout__currency", currency);
+  successParams.set("checkout__frequency", frequency);
+  successParams.set("checkout__amount", amount);
+  successParams.set("checkout__country", country);
+  successParams.set("checkout__locale", locale);
+  successParams.set("checkout__timestamp", clickTs);
   successUrl = `${successUrl}?${successParams.toString()}`;
 
   const customData = {
