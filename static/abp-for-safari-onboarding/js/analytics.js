ecd08d39579ddea4011825926da032438faa6c6b
Adrian Cotfas <a.cotfas@eyeo.com>
2025-04-10 16:45:32 +0300

    feat: add ABP for Safari extension onboarding pages

---
+++ static/abp-for-safari-onboarding/js/analytics.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAnalytics, setUserId } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAZAdrmwP9-sbYzIPFsQrYNfWRCnhAlCkE",
+    authDomain: "eyeo-nasapp.firebaseapp.com",
+    databaseURL: "https://eyeo-nasapp-default-rtdb.firebaseio.com",
+    projectId: "eyeo-nasapp",
+    storageBucket: "eyeo-nasapp.appspot.com",
+    messagingSenderId: "319546247729",
+    appId: "1:319546247729:web:45c4c172da5ab0b20aa310",
+    measurementId: "G-1ZMGMVYFSK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+// Read from URL
+const params = new URLSearchParams(window.location.search);
+const userId = params.get('user_id');
+
+if (userId) {
+    setUserId(analytics, userId);
+}
---
+++ static/abp-for-safari-onboarding/js/replace-inline-images.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.querySelector('.content');
+    const html = content.innerHTML;
+
+    // Replace image placeholders with actual img tags
+    content.innerHTML = html.replace(/\[\[IMG:([^\]]+)]]/g, (match, imgName) => {
+        return `<img src="/abp-for-safari-onboarding/img/${imgName}.svg" alt="${imgName.replace('_', ' ')}">`;
+    });
+});
