c5b813ddab05a362293fc78e5e145570f473be75
Carly Lilwall <c.lilwall@adblock.org>
2025-07-08 20:27:01 +0100

    Add Chrome and Edge tests

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-	await page.goto("");
+	await page.goto('');
 });
 
 test('Cookie Banner link', async ({ page, browserName }) => {
@@ -10,19 +10,29 @@ test('Cookie Banner link', async ({ page, browserName }) => {
 	expect(page.url()).toContain('/block-cookie-banners?s=hpban');
 });
 
-test('Extension download link', async ({ page, browserName }) => {
-	switch (browserName) {
-	case "firefox":
-		await page.locator("#install-button").click();
+test('Extension download link', async ({ page, browserName, channel }) => {
+	if (browserName !== 'webkit') {
+		await page.locator('#install-button').click();
+	}
+	switch(browserName) {
+	case 'firefox':
 		await expect(page).toHaveURL('https://addons.mozilla.org/en-US/firefox/addon/adblock-plus/');
 		break;
-	case "webkit":
+	case 'webkit':
 		// Playwright cannot interact with App Store so check link is correct without clicking
-		await expect(page.locator("#install-button")).toHaveAttribute('href', 'https://apps.apple.com/us/app/adblock-plus-for-safari-abp/id1432731683');
+		await expect(page.locator('#install-button')).toHaveAttribute('href', 'https://apps.apple.com/us/app/adblock-plus-for-safari-abp/id1432731683');
+		break;
+	case 'Microsoft Edge':
+		await expect(page).toHaveURL('https://microsoftedge.microsoft.com/addons/detail/adblock-plus-free-ad-bl/gmgoamodcdcjnbaobigkjelfplakmdhh');
 		break;
 	default:
-		await page.locator("#install-button").click();
-		await expect(page).toHaveURL('https://chromewebstore.google.com/detail/adblock-plus-free-ad-bloc/cfhdojbkjhnklbpkdaibdccddilifddb');
+		switch(channel) {
+			case 'msedge':
+				await expect(page).toHaveURL('https://microsoftedge.microsoft.com/addons/detail/adblock-plus-free-ad-bl/gmgoamodcdcjnbaobigkjelfplakmdhh');
+				break;
+		default:
+			await expect(page).toHaveURL('https://chromewebstore.google.com/detail/adblock-plus-free-ad-bloc/cfhdojbkjhnklbpkdaibdccddilifddb');
+		}
 	};
 });
 
@@ -34,14 +44,15 @@ test('Adblock Browser download link', async ({ page }) => {
 [
 	{ pageLink: 'learn more', expectedUrl: '/acceptable-ads' },
 	{ pageLink: '1', expectedUrl: '/#footnote' },
-//	{ pageLink: 'GPLv3+', expectedUrl: 'https://www.gnu.org/licenses/gpl-3.0.html' },
+	{ pageLink: 'GPLv3+', expectedUrl: 'https://www.gnu.org/licenses/gpl-3.0.html' },
 	{ pageLink: 'Terms of Use', expectedUrl: '/terms' },
 	{ pageLink: 'Download Adblock Plus for another browser', expectedUrl: '/download' },
 	{ pageLink: 'Learn how', expectedUrl: '/acceptable-ads#optout' },
 	{ pageLink: 'criteria', expectedUrl: '/acceptable-ads#criteria-general' },
 	{ pageLink: 'forum', expectedUrl: 'https://forum.adblockplus.org//viewforum.php?f=12' },
 ].forEach(({ pageLink, expectedUrl }) => {
-	test("Page links: " + pageLink, async ({ page }) => {
+	test('Page links: ' + pageLink, async ({ page }) => {
+		test.skip(pageLink === 'GPLv3+', 'gnu.org is currently down');
 		await page.getByRole('link', { name: pageLink, exact: true }).click();
   		expect(page.url()).toContain(expectedUrl);
 	});
@@ -54,7 +65,7 @@ test('Adblock Browser download link', async ({ page }) => {
 	{ pageLink: 'New York Times logo', expectedUrl: 'https://www.nytimes.com/2015/08/20/technology/personaltech/ad-blockers-and-the-nuisance-at-the-heart-of-the-modern-web.html' },
 	{ pageLink: 'Media post logo', expectedUrl: 'https://www.mediapost.com/publications/article/289691/adblock-plus-comes-to-new-york.html' },
 ].forEach(({ pageLink, expectedUrl }) => {
-	test("As mentioned on links: " + pageLink, async ({ page, browserName }) => {
+	test('As mentioned on links: ' + pageLink, async ({ page, browserName }) => {
 		test.skip(browserName !== 'chromium', 'Only need to test third party links on Chromium');
 		const pagePromise = page.waitForEvent('popup');
 		await page.getByRole('link', { name: pageLink }).click();
