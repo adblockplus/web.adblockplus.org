1e0e61df30226b8b787a6e206d04a9c4be5e35c6
Alex Gibson <alexgibson@users.noreply.github.com>
2025-11-28 09:55:03 +0000

    chore: remove optimizely snippet

--- tests/test-helpers/url-helper.js
+++ tests/test-helpers/url-helper.js
@@ -3,7 +3,7 @@ export class URLHelper {
 
   static async addURLParameter(URL, Parameter) {
     let testURL = URL;
-    testURL = URL + '?testmode&optimizely_disable=true&experiment_disable=true';
+    testURL = URL + '?testmode&experiment_disable=true';
     if (Parameter != '') {
       testURL = testURL + '&' + Parameter;
     }
