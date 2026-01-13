3d0212021c6ef793fd986e6f0d6dba25afdb10c6
CLilwall <c.lilwall@eyeo.com>
2026-01-13 12:46:38 +0000

    Correct health endpoints

--- tests/global.setup.js
+++ tests/global.setup.js
@@ -1,7 +1,8 @@
 import { test as setup } from '@playwright/test';
 
 // QA User Accounts needs to initialise before tests run or else first test may time out
-// Calling the health endpoint wakes the API
+// Calling the health endpoints wakes the API
 setup('Initialise User Accounts', async ({ page }) => {
-  await page.goto('https://abp.ua-qa.eyeo.it/v1/health');
+  await page.goto('https://api.ua-qa.eyeo.it/v1/health');
+  await page.goto('https://webhook.ua-qa.eyeo.it/health');
 });
